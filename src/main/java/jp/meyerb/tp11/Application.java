37629785ef604996aab0ff04494d5b340979533e
ReyeM <brice.meyer@imie.fr>
2018-10-18 12:07:22 +0200

    start project

---
+++ src/main/java/jp/meyerb/tp11/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jp.meyerb.tp11;
+
+/**
+ * @author Brice
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
